df0b9cf8a2fbee8e67f58220ad3d71dcd43ddd2c
P1zhama <vlad-serdjukov@bk.ru>
2026-08-25 05:55:43 +0300

    add IClosedPeriodRepository

--- backendProject/src/Project.Application/Common/Interfaces/IRepositories.cs
+++ backendProject/src/Project.Application/Common/Interfaces/IRepositories.cs
@@ -1,10 +1,23 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Project.Domain.Entities;
+using Project.Domain.ValueObjects;
 
 namespace Project.Application.Common.Interfaces
-{
+{   
+    public interface IClosedPeriodRepository
+    {
+        Task<bool> IsClosedAsync(YearMonth period, CancellationToken cancellationToken);
+
+        Task<IReadOnlyList<ClosedPeriod>> GetAllAsync(CancellationToken cancellationToken);
+
+        Task<bool> CloseAsync(YearMonth period, DateTime closedAt, string id, CancellationToken cancellationToken);
+
+        Task<bool> OpenAsync(YearMonth period, CancellationToken cancellationToken);
+    }
+
     public interface ITimeEntryRepository
     {
         Task<TimeEntry?> GetByIdAsync(string id, CancellationToken cancellationToken);
